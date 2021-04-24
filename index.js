0c107c82a642f86281f2acc4b97b37c8c98b64b8
jypara-git <zhyparaabdieva@gmail.com>
2021-04-24 13:13:37 -0700

    add an employee

--- index.js
+++ index.js
@@ -71,28 +71,71 @@ const promptUser = () => {
                 const departmentList = rows.map(textRow => textRow.name);
                 promptRole(departmentList)
                 .then((roleResponse) => {
-                    const sql = `SELECT id FROM departments WHERE name=?`;
-                    const params = roleResponse.department_name;
-                    db.query(sql,params,(err,rows)=>{
+                    const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, (SELECT id FROM departments WHERE name=?))`;
+                    const params = [roleResponse.title, roleResponse.salary, roleResponse.department_name];
+                    db.query(sql, params, (err, _)=>{
                         if (err) {
                             console.log(err);
                             return;
                         }
-                        const dep_id = rows[0].id;
-                        const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`;
-                        const params = [roleResponse.title, roleResponse.salary, dep_id];
-                        console.log(params);
-                        db.query(sql, params, (err, _) => {
-                            if (err) {
-                                console.log(err);
-                                return;
-                            }
-                            console.log(roleResponse.title + ' added to roles!');
-                            promptUser();
-                        })
+                        console.log(roleResponse.title + ' added to roles!');
+                        promptUser();
                     })
                 })
             })
+        } else if (option.options === 'Add an employee') {
+            const sql = `SELECT title FROM roles`;
+            db.query(sql, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                const rolesList = rows.map(textRow => textRow.title); 
+                const sql2 = `SELECT first_name, last_name FROM employees`;
+                db.query(sql2, (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    const employeesList = rows.map(textRow => textRow.first_name + ' ' +  textRow.last_name);
+                    employeesList.push('None');
+                    promptEmployee(rolesList, employeesList)
+                    .then((promptValues) => {
+                        if ( promptValues.manager === 'None') {
+                            const sql = `INSERT INTO employees (first_name, last_name, role_id) VALUES (?, ?,(SELECT id FROM roles WHERE title=?))`;
+                            const params = [promptValues.first_name, promptValues.last_name, promptValues.roles];
+                            db.query(sql, params, (err, result) => {
+                                if (err) {
+                                    console.log(err);
+                                    return;
+                                }
+                                console.log('Employee was added!');
+                                promptUser();
+                            })
+                        } else {
+                            const sql = `SELECT id FROM employees WHERE first_name=? and last_name=?`;
+                            const params = promptValues.manager.split(' ');
+                            db.query(sql, params, (err, rows) => {
+                                if (err) {
+                                    console.log(err);
+                                    return;
+                                }
+                                const manager_id = rows[0].id;
+    
+                                const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?,(SELECT id FROM roles WHERE title=?), ?)`;
+                                const params = [promptValues.first_name, promptValues.last_name, promptValues.roles, manager_id];
+                                db.query(sql, params, (err, result) => {
+                                    if (err) {
+                                        console.log(err);
+                                        return;
+                                    }
+                                    console.log('Employee was added!');
+                                    promptUser();
+                                })
+                            })
+                        }
+                    })
+                });
+            });
         }
     });
 };
@@ -144,6 +187,47 @@ const promptRole = (departmentList) => {
             name: 'department_name',
             message: 'What is the department?',
             choices: departmentList
+        }
+    ])
+};
+
+const promptEmployee = (rolesList, employeesList) => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "What is the employee's name?",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                }
+                console.log('Please enter the name!');
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "What is the employee's last name?",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                }
+                console.log('Please enter last name!');
+                return false;
+            }
+        },
+        {
+            type: 'list',
+            name: 'roles',
+            message: "What is the employee's role?",
+            choices: rolesList
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: "Who is the employee's manager?",
+            choices: employeesList
         }
     ])
 }
